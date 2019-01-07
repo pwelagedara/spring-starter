a8808707f551bac9f3b7ea8a06e6c604730e7849
pwelagedara <pubuduwelagedara@gmail.com>
2019-01-06 20:44:31 -0500

    added jndi configuration

---
+++ src/main/java/online/pubudu/springstarter/ServletInitializer.java
@@ -0,0 +1,14 @@
+package online.pubudu.springstarter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringStarterApplication.class);
+	}
+
+}
+
