f6a72cbc4a7b7b049ddd76dc90f9864938edd5f3
pwelagedara <pubuduwelagedara@gmail.com>
2019-01-07 22:41:11 -0500

    fixing gradle profile issues

--- src/main/java/online/pubudu/springstarter/SpringStarterApplication.java
+++ src/main/java/online/pubudu/springstarter/SpringStarterApplication.java
@@ -2,11 +2,14 @@ package online.pubudu.springstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /*
 * Created by pubudu welagedara on 12/17/18.
 * */
-@SpringBootApplication
+// https://stackoverflow.com/questions/28747909/how-to-disable-spring-data-mongodb-autoconfiguration-in-spring-boot
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SpringStarterApplication {
 
 	public static void main(String[] args) {
@@ -14,4 +17,3 @@ public class SpringStarterApplication {
 	}
 
 }
-
