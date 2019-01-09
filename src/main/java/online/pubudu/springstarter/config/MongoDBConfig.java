946e3514604f08dda81ded80d178b0ce609d5a2b
pwelagedara <pubuduwelagedara@gmail.com>
2019-01-08 22:00:22 -0500

    removed mongodb stuff

--- src/main/java/online/pubudu/springstarter/config/MongoDBConfig.java
+++ src/main/java/online/pubudu/springstarter/config/MongoDBConfig.java
@@ -1,7 +1,6 @@
 package online.pubudu.springstarter.config;
 
 import com.mongodb.MongoClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
